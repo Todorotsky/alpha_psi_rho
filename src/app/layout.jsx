1e5a3b99216a714d376e4698751edb4769a90a55
Caleb Wilson <alexanderzyn1@gmail.com>
2024-02-13 15:25:04 -0800

    fix shadcn import error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar.jsx"
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +13,8 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				<Navbar />
-				{children}</body>
+				{children}
+			</body>
 		</html>
 	);
 }
