94b2a9a6935cc089c86c906fb239db991df4894e
Troy Galicia <85812562+Todorotsky@users.noreply.github.com>
2024-02-13 16:47:22 -0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const Navbar = () => {
@@ -5,5 +6,106 @@ const Navbar = () => {
     <div>Navbar</div>
   )
 }
+=======
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-4 pr-16">
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/Apsirhocrest.jpeg"
+          width={75}
+          height={75}
+          alt="Apsirho Crest"
+        />
+        <div className="leading-none font-bold text-xl">
+          <h1>
+            Alpha Psi Rho
+            <br />
+            Delta Chapter
+          </h1>
+        </div>
+      </Link>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              About
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink asChild>Mission Statement</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Join Us
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              News
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Brhothers
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Media
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Contact
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+>>>>>>> Stashed changes
 
 export default Navbar
