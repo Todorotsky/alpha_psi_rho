3803d2141e6e16d157bc350f78a83373dbf3d426
Troy Galicia <galict1@unlv.nevada.edu>
2024-03-01 22:06:54 -0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
   NavbarMenuToggle,
   Link,
   Button,
@@ -22,12 +24,118 @@ const Navigationbar = () => {
     chevron: <ChevronDown fill="currentColor" size={16} />,
   };
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
     <Navbar
       className="w-[100%] sm:w-[72%] ml-0 mr-0 sm:ml-[14%] sm:mr-[14%] flex justify-start"
       isBlurred="false"
       maxWidth={"full"}
     >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+      </NavbarContent>
+      <NavbarMenu className=" w-[60%]">
+        <NavbarMenuItem className="pt-2">
+          <Link className="text-3xl" href="/" aria-current="page">
+            Home
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="pt-2">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="text-3xl p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={icons.chevron}
+                radius="sm"
+                variant="light"
+              >
+                About
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="About Us"
+              className="w-[100%]"
+              itemClasses={{
+                base: "gap-4",
+              }}
+            >
+              <DropdownItem
+                key="About Us"
+                description="Pillars and Mission Statement"
+                startContent={icons.scale}
+                href="/about"
+              >
+                About Us
+              </DropdownItem>
+              <DropdownItem
+                key="Chapters"
+                description="Universities"
+                startContent={icons.scale}
+                href="/chapters"
+              >
+                Chapters
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <Dropdown>
+            <NavbarMenuItem className="pt-2">
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  className="text-3xl p-0 bg-transparent data-[hover=true]:bg-transparent"
+                  endContent={icons.chevron}
+                  radius="sm"
+                  variant="light"
+                >
+                  Join Us
+                </Button>
+              </DropdownTrigger>
+            </NavbarMenuItem>
+            <DropdownMenu
+              aria-label="Join Us"
+              className="w-[100%]"
+              itemClasses={{
+                base: "gap-4",
+              }}
+            >
+              <DropdownItem
+                key="APsiRho101"
+                startContent={icons.scale}
+                description="Powerpoint About APsiRho"
+                href="/about"
+              >
+                APsiRho101
+              </DropdownItem>
+              <DropdownItem
+                key="How to Join"
+                description="How-To and Requirements"
+                startContent={icons.scale}
+                href="/join"
+              >
+                How to Join
+              </DropdownItem>
+              <DropdownItem
+                key="Interest Form"
+                description="Google Form"
+                startContent={icons.scale}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeE7k9E0oweC9APcmSWvnFHIo9nQ2WiD0H3aYQRySa7lg3ATw/viewform"
+              >
+                Interest Form
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="pt-2">
+          <Link className="text-3xl" href="/contact" aria-current="page">
+            Contact
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+
       <NavbarBrand justify="start">
         <div>
           <Link href="/" className="flex items-center">
