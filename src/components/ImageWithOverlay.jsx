89cc9ce78f09bd9a8cf870682a8a0e58aecf6386
Troy Galicia <85812562+Todorotsky@users.noreply.github.com>
2024-03-15 19:21:35 -0700

    Minor homepage fix

--- src/components/ImageWithOverlay.jsx
+++ src/components/ImageWithOverlay.jsx
@@ -18,7 +18,7 @@ const ImageWithOverlay = ({
       />
       <div className="absolute right-0 bottom-[8%] mr-2 sm:mr-14 mt-6 w-[61%] h-[92%] justify-center">
         <div className="bg-black bg-opacity-50 rounded-full h-[92%] flex flex-col items-center p-[1vh]">
-          <div className="mt-[1vw] mb-[-1vw]">
+          <div className="mt-[3vw] sm:mt-[5vw] mb-[-1vw] sm:mb-[1vw]">
             <span className="text-white text-2xl sm:text-[4vw] font-bold">
               {headerText}
             </span>
